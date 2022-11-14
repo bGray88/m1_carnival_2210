e1799eac1917b3cd964fdc142eee468e60820d89
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-14 09:29:46 -0700

    feat: adds ride with attributes

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,14 @@
 class Ride
 
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement
+              
+  def initialize(args)
+    @name          = args[:name]
+    @min_height    = args[:min_height]
+    @admission_fee = args[:admission_fee]
+    @excitement    = args[:excitement]
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,7 +5,14 @@ require_relative '../lib/visitor'
 require 'rspec'
 
 describe Ride do
-  let(:ride1)    { Ride.new() }
+  let(:ride1)    {
+                   Ride.new({
+                              name: 'Carousel',
+                              min_height: 24,
+                              admission_fee: 1,
+                              excitement: :gentle
+                            })
+                  }
   let(:visitor1) { Visitor.new('Bruce',  54, '$10') }
   let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
   let(:visitor3) { Visitor.new('Penny',  64, '$15') }
@@ -13,6 +20,10 @@ describe Ride do
   describe '#initialize' do
     it 'exists and has attributes' do
       expect(ride1).to be_instance_of(Ride)
+      expect(ride1.name).to eq('Carousel')
+      expect(ride1.min_height).to eq(24)
+      expect(ride1.admission_fee).to eq(1)
+      expect(ride1.excitement).to eq(:gentle)
     end
   end
 end
