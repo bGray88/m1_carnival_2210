34e6517f154e0dfb40d09183edcce6cb45aa8ed6
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-14 09:14:36 -0700

    feat: add preference

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -3,7 +3,7 @@ class Visitor
   attr_reader :name,
               :height,
               :preferences
-              
+
   def initialize(name, height, spending_money)
     @name           = name
     @height         = height
@@ -13,5 +13,9 @@ class Visitor
 
   def spending_money
     @spending_money.delete('$').to_f.round(2)
+  end
+
+  def add_preference(preference)
+    preferences.push(preference)
   end
 end
