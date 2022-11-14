cf2b62853d2ba1eb7381081256e21bb17008ca2f
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-14 09:11:12 -0700

    feat: adds visitor spending money and preferences

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,9 +1,17 @@
 class Visitor
 
   attr_reader :name,
-              :height
-  def initialize(name, height)
-    @name   = name
-    @height = height
+              :height,
+              :preferences
+              
+  def initialize(name, height, spending_money)
+    @name           = name
+    @height         = height
+    @spending_money = spending_money
+    @preferences    = []
+  end
+
+  def spending_money
+    @spending_money.delete('$').to_f.round(2)
   end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -4,13 +4,15 @@ require_relative '../lib/visitor'
 
 describe Visitor do
   
-  let(:visitor1) { Visitor.new('Stella', '5\'6\"') }
+  let(:visitor1) { Visitor.new('Stella', '5\'6\"', '$10') }
 
   describe '#initialize' do
     it 'exists and has attributes' do
       expect(visitor1).to be_instance_of(Visitor)
       expect(visitor1.name).to eq('Stella')
       expect(visitor1.height).to eq('5\'6\"')
+      expect(visitor1.spending_money).to eq(10)
+      expect(visitor1.preferences).to eq([])
     end
   end
 end
