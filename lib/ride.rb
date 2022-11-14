e1b561514030dcf9d4b5c269a7d670e4cc10f97a
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-14 09:30:52 -0700

    feat: adds ride totlal revenue

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,12 +3,14 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
-              
+              :excitement,
+              :total_revenue
+
   def initialize(args)
     @name          = args[:name]
     @min_height    = args[:min_height]
     @admission_fee = args[:admission_fee]
     @excitement    = args[:excitement]
+    @total_revenue = 0
   end
 end
